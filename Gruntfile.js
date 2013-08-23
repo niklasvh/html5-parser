a58bdf062a4f56dcb2f7c037b0d87ad20767ddda
Niklas von Hertzen <niklasvh@gmail.com>
2013-08-23 19:50:58 +0300

    Add browser version

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,79 +2,88 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-    // Task configuration.
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
-      },
-      dist: {
-        src: ['lib/<%= pkg.name %>.js'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'dist/<%= pkg.name %>.min.js'
-      }
-    },
-    nodeunit: {
-      files: ['test/**/*_test.js']
-    },
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib: {
-        options: {
-          jshintrc: 'lib/.jshintrc'
+    // Project configuration.
+    grunt.initConfig({
+        // Metadata.
+        pkg: grunt.file.readJSON('package.json'),
+        browserify: {
+            files: {
+                src: ['lib/tokenizer.js'],
+                dest: 'build/tokenizer.js',
+                options: {
+                    alias: ['lib/tokenizer.js:Parser']
+                }
+            }
         },
-        src: ['lib/**/*.js']
-      },
-      test: {
-        src: ['test/**/*.js']
-      }
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib: {
-        files: '<%= jshint.lib.src %>',
-        tasks: ['jshint:lib', 'nodeunit']
-      },
-      test: {
-        files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test', 'nodeunit']
-      }
-    }
-  });
+        concat: {
+            options: {
+                banner: '/*\n  <%= pkg.title || pkg.name %> <%= pkg.version %>' +
+                    '<%= pkg.homepage ? " <" + pkg.homepage + ">" : "" %>' + '\n' +
+                    '  Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>' +
+                    '\n\n  Released under <%= _.pluck(pkg.licenses, "type").join(", ") %> License\n*/\n' +
+                    '(function(window, document, undefined) {\n',
+                footer: 'window.Parser = require("Parser").Parser;' +
+                    '\n})(window, document);',
+                stripBanners: true
+            },
+            dist: {
+                src: ['build/tokenizer.js'],
+                dest: 'build/tokenizer.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            dist: {
+                src: '<%= concat.dist.dest %>',
+                dest: 'dist/<%= pkg.name %>.min.js'
+            }
+        },
+        nodeunit: {
+            files: ['test/**/*_test.js']
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            lib: {
+                options: {
+                    jshintrc: 'lib/.jshintrc'
+                },
+                src: ['lib/**/*.js']
+            },
+            test: {
+                src: ['test/**/*.js']
+            }
+        },
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile']
+            },
+            lib: {
+                files: '<%= jshint.lib.src %>',
+                tasks: ['jshint:lib', 'nodeunit']
+            },
+            test: {
+                files: '<%= jshint.test.src %>',
+                tasks: ['jshint:test', 'nodeunit']
+            }
+        }
+    });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'nodeunit', 'concat', 'uglify']);
+    // Default task.
+    grunt.registerTask('default', ['jshint', 'nodeunit', 'browserify', 'concat']);
 
 };
